5ad4f0bfb20235e1ebecee96f1d528d788e4b45c
Sean Aguilar <seanaguilar698@gmail.com>
2024-02-25 16:38:52 +0800

    fixed paths

--- src/components/ModeToggle.jsx
+++ src/components/ModeToggle.jsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "./components/ui/dropdown-menu";
+} from "./ui/dropdown-menu";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
