c54098667a8cca96c0d446404f0bf5a61cbde38a
Sean Aguilar <seanaguilar698@gmail.com>
2024-02-25 17:43:50 +0800

    fixed import path

--- src/app/sender/page.jsx
+++ src/app/sender/page.jsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../lib/auth";
+import { authOptions } from "../../lib/auth";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
