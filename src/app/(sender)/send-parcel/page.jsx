4c089cac2bd3c7907d120c14648522d6aeeb849d
Sean Aguilar <seanaguilar698@gmail.com>
2024-03-19 02:17:02 +0800

    Update send parcel form

--- src/app/(sender)/send-parcel/page.jsx
+++ src/app/(sender)/send-parcel/page.jsx
@@ -1,42 +1,28 @@
 "use client";
 import { redirect, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Input } from "../../../components/ui/input";
 import { Button } from "../../../components/ui/button";
-import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
-
+import { MoreHorizontal } from "lucide-react";
 import {
   Form,
   FormControl,
-
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "../../../components/ui/form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../../../components/ui/card";
-
+import { Card, CardHeader } from "../../../components/ui/card";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "../../../components/ui/table";
-
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -44,8 +30,10 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "../../../components/ui/dropdown-menu";
+import { Label } from "../../../components/ui/label";
+import { useItem } from "../../api/useItem";
+import { useEffect, useState } from "react";
 
-/*===========================================================*/
 const FormSchema = z.object({
   receiverName: z.string().min(3, {
     message: "Name must be at least 3 characters.",
@@ -63,7 +51,14 @@ const FormSchema = z.object({
 
 export default function SendParcel() {
   const { data: session } = useSession();
-  // const router = useRouter();
+  const { createItem, fetchItems, fetchItem, updateItem, removeItem } =
+    useItem();
+  const [itemData, setItemData] = useState({
+    itemName: "",
+    itemQuantity: 0,
+    itemPrice: 0,
+  });
+  const [items, setItems] = useState([]);
 
   if (!session || session.user.role !== "sender") {
     return redirect("/");
@@ -81,6 +76,14 @@ export default function SendParcel() {
     },
   });
 
+  useEffect(() => {
+    const getItems = async () => {
+      const itemsData = await fetchItems();
+      setItems(itemsData);
+    };
+    getItems();
+  }, [fetchItems]);
+
   //FOR CONSULTATION
   function processOrder(data) {
     //processes order.
@@ -89,17 +92,45 @@ export default function SendParcel() {
       receiverEmail: data.receiverName,
       receiverAddress: data.receiverAddress,
     };
-
-    console.log(userData);
   }
 
-  function addItem() {
-    //adds added item into the table.
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setItemData((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  async function handleItem(e) {
+    e.preventDefault();
+
+    try {
+      if (!itemData.itemName || !itemData.itemQuantity || !itemData.itemPrice) {
+        throw new Error("All fields are required");
+      }
+
+      const itemRef = await createItem(itemData);
+
+      setItemData({
+        itemName: "",
+        itemQuantity: 0,
+        itemPrice: 0,
+      });
+    } catch (error) {
+      console.error("Error adding item:", error.message);
+    }
   }
+
+  const handleRemoveItem = async (item) => {
+    await removeItem(item.id);
+    const updatedItems = await fetchItems();
+    setItems(updatedItems);
+  };
 
   return (
     <>
-      <Card className="m-12 ">
+      <Card className="m-12">
         <Form {...form}>
           <form onSubmit={form.handleSubmit(processOrder)}>
             <Card className="m-8">
@@ -153,96 +184,80 @@ export default function SendParcel() {
 
             <Card className="m-8">
               <CardHeader>Parcel Contents</CardHeader>
-              <div className="flex max-md:flex-col mb-6">
-                <FormField
-                  className="py-4 mx-3"
-                  control={form.control}
-                  name="itemName"
-                  render={({ field }) => (
-                    <>
-                      <FormItem className=" w-screen mx-6 max-md:w-full max-md:pr-[55px]">
-                        <FormLabel>item:</FormLabel>
-                        <FormControl>
-                          <Input type="string" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    </>
-                  )}
-                />
-                <FormField
-                  className="py-4 mx-3"
-                  control={form.control}
-                  name="itemQuanity"
-                  render={({ field }) => (
-                    <>
-                      <FormItem className=" w-screen mx-6 max-md:w-full max-md:pr-[55px]">
-                        <FormLabel>quantity:</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    </>
-                  )}
-                />
-                <FormField
-                  className="py-4 mx-3"
-                  control={form.control}
-                  name="itemPrice"
-                  render={({ field }) => (
-                    <>
-                      <FormItem className=" w-screen mx-6 max-md:w-full max-md:pr-[55px]">
-                        <FormLabel>price:</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                      </FormItem>
-                    </>
-                  )}
-                />
+              {/* w-screen mx-6 max-md:w-full max-md:pr-[55px] */}
+              <div className="flex justify-between py-4 mx-3 max-md:flex-col">
+                <div className="mx-3  w-screen max-md:w-full max-md:pe-3">
+                  <Label htmlFor="itemName">item:</Label>
+                  <Input
+                    type="text"
+                    id="itemName"
+                    value={itemData.itemName}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="mx-3  w-screen max-md:w-full max-md:pe-3">
+                  <Label htmlFor="quantity">quantity:</Label>
+                  <Input
+                    type="number"
+                    id="itemQuantity"
+                    value={itemData.itemQuantity}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="mx-3  w-screen max-md:w-full max-md:pe-3">
+                  <Label htmlFor="price">price:</Label>
+                  <Input
+                    type="number"
+                    id="itemPrice"
+                    value={itemData.itemPrice}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
 
               <div className="flex justify-end mr-6 mb-3 max-md:mr-8">
-                <Button type="submit" value="Submit" className="">
-                  Add Item
-                </Button>
+                <Button onClick={handleItem}>Add Item</Button>
               </div>
 
-              <Table className>
+              <Table>
                 <TableHeader>
                   <TableRow>
                     <TableHead>Item</TableHead>
-
-                    <TableHead>quantity</TableHead>
-
-                    <TableHead className="text-right">price</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead className="text-right">Price</TableHead>
+                    <TableHead></TableHead>
                   </TableRow>
                 </TableHeader>
-
                 <TableBody>
-                  <TableRow>
-                    <TableCell>data1</TableCell>
-                    <TableCell>data2</TableCell>
-                    <TableCell className="text-right">data3</TableCell>
-                    <TableCell>
-                      <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                          <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                          </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent align="end">
-                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                          <DropdownMenuItem onClick={() => null}>
-                            Edit content
-                          </DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => null}>
-                            Delete content
-                          </DropdownMenuItem>
-                        </DropdownMenuContent>
-                      </DropdownMenu>
-                    </TableCell>
-                  </TableRow>
+                  {items.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{item.itemName}</TableCell>
+                      <TableCell>{item.itemQuantity}</TableCell>
+                      <TableCell className="text-right">
+                        {item.itemPrice}
+                      </TableCell>
+                      <TableCell>
+                        <DropdownMenu>
+                          <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                              <span className="sr-only">Open menu</span>
+                              <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem
+                              onClick={() => handleRemoveItem(item)}
+                            >
+                              Delete content
+                            </DropdownMenuItem>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </TableCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
             </Card>
