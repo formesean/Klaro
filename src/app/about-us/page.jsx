04b36790466a00e2cd2f3b34dab0dc89adbfd8a0
formesean <seanaguilar698@gmail.com>
2024-04-21 20:14:09 +0800

    Add dynamic rendering for team members

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,80 +1,17 @@
-"use client";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import dynamic from "next/dynamic";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../../components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "../../components/ui/table";
-import { Input } from "../../components/ui/input";
-import { Button } from "../../components/ui/button";
-import { Separator } from "../../components/ui/separator";
-import {
-  ArrowUpDown,
-  Divide,
-  Loader,
-  MoreHorizontal,
-  Radio,
-} from "lucide-react";
-import { useEffect, useState } from "react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "../../components/ui/dropdown-menu";
-import {
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel,
-  getPaginationRowModel,
-  getSortedRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import { Label } from "@radix-ui/react-dropdown-menu";
-import { DeliveryStatus } from "../dashboard/@sender/components/DeliveryStatus";
-import {
-  RadioGroup,
-  RadioGroupItemWithIcons,
-} from "../../components/ui/radio-group";
 import Image from "next/image";
-import uy_about_us from "../../../public/uy_about_us.png";
-import aguilar_about_us from "../../../public/aguilar_about_us.png";
-import canos_about_us from "../../../public/canos_about_us.png";
-import ochia_about_us from "../../../public/ochia_about_us.png";
 import { Linkedin, Github, Mail } from "lucide-react";
+import { TeamData } from "../links";
+import { Separator } from "../../components/ui/separator";
 
-export default function Dashboard() {
-  const { data: session } = useSession();
-
-  if (!session && session?.user.role !== "deliveryService") {
-    return redirect("/");
-  }
-
+export default function AboutUs() {
   return (
-    <div className="">
+    <>
       <div className="flex flex-col">
-        <div className="my-4 ">
-          <div className="">
-            <p className="text-center text-6xl font-bold">WE ARE KLARO</p>
-          </div>
-        </div>
-        <div className="">
-          <div className="flex my-8 align-items-center place-items-center justify-center">
-            <p className="text-center max-w-[1400px] pl-7 pr-7">
+        <div className="flex flex-col gap-6 my-6">
+          <p className="text-center text-6xl font-bold">WE ARE KLARO</p>
+
+          <div className="flex align-items-center place-items-center justify-center">
+            <p className="text-center max-w-[1000px] px-8">
               Embark on a daring journey with Klaro, where four college comrades
               converge to reinvent parcel tracking. Fueled by late-night pizza
               and boundless creativity, we're reshaping shipping with coding
@@ -84,154 +21,56 @@ export default function Dashboard() {
             </p>
           </div>
         </div>
-        <div className="border border-x-0 flex flex-col">
-          <div className="">
-            <div className="my-2.5">
-              <h1 className="text-xl font-bold text-center">Meet The Team</h1>
-            </div>
-            <div className="my-3 flex align-items-center place-items-center justify-center gap-10 max-xl:flex-col">
-              <div className="flex flex-col">
+
+        <Separator className="my-4" />
+
+        <div className="flex flex-col">
+          <h1 className="text-xl font-bold text-center">Meet The Team</h1>
+
+          <div className="flex align-items-center place-items-center justify-center gap-10 max-lg:gap-0 max-lg:flex-col">
+            {TeamData.map((member, index) => (
+              <div className="flex flex-col" key={index}>
                 <div className="m-4">
                   <Image
-                    src={aguilar_about_us}
+                    src={member.image}
                     width={180}
-                    alt="Picture of Sean Aguilar"
+                    height={180}
+                    alt={`Picture of ${member.name}`}
+                    priority={true}
                   />
                   <div className="flex flex-col align-items-center place-items-center justify-center">
                     <p className="text-white font-bold text-lg py-3">
-                      Sean Aguilar
+                      {member.name}
                     </p>
                     <div className="flex gap-2">
-                      <a
-                        href="https://github.com/formesean"
-                        target="_blank"
-                        title="GitHub"
-                      >
-                        <Github className="hover:animate-bounce hover:stroke-[#22C55E]" />
+                      <a href={member.github} target="_blank" title="GitHub">
+                        <Github className="hover:scale-125 transition-all duration-500 hover:stroke-[#22C55E]" />
                       </a>
-                      <a href="mailto:seanaguilar698@gmail.com" title="Gmail">
-                        <Mail className="hover:animate-bounce hover:stroke-[#22C55E]" />
+                      <a href={member.mail} title="Gmail">
+                        <Mail className="hover:scale-125 transition-all duration-500 hover:stroke-[#22C55E]" />
                       </a>
                       <a
-                        href="https://www.linkedin.com/in/seanaguilar04"
+                        href={member.linkedin}
                         target="_blank"
                         title="LinkedIn"
                       >
-                        <Linkedin className="hover:animate-bounce hover:stroke-[#22C55E]" />
+                        <Linkedin className="hover:scale-125 transition-all duration-500 hover:stroke-[#22C55E]" />
                       </a>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="flex flex-col">
-                <div className="m-4">
-                  <Image
-                    src={canos_about_us}
-                    width={180}
-                    alt="Picture of Bea Canos"
-                  />
-                  <div className="flex flex-col align-items-center place-items-center justify-center">
-                    <p className="text-white font-bold text-lg py-3">
-                      Bea Canos
-                    </p>
-                    <div className="flex gap-2">
-                      <a
-                        href="https://github.com/vaebelle"
-                        target="_blank"
-                        title="GitHub"
-                      >
-                        <Github className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a href="mailto:beabelle.canos@gmail.com" title="Gmail">
-                        <Mail className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a
-                        href="https://www.linkedin.com/in/beabellecanos"
-                        target="_blank"
-                        title="LinkedIn"
-                      >
-                        <Linkedin className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <div className="m-4">
-                  <Image
-                    src={ochia_about_us}
-                    width={180}
-                    alt="Picture of May Ochia"
-                  />
-                  <div className="flex flex-col align-items-center place-items-center justify-center">
-                    <p className="text-white font-bold text-lg py-3">
-                      May Ochia
-                    </p>
-                    <div className="flex gap-2">
-                      <a
-                        href="https://github.com/Mayochia"
-                        title="GitHub"
-                        target="_blank"
-                      >
-                        <Github className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a href="mailto:mayochia5@gmail.com" title="Gmail">
-                        <Mail className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a
-                        href="https://www.linkedin.com/in/may-ochia-086267270/"
-                        title="LinkedIn"
-                        target="_blank"
-                      >
-                        <Linkedin className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col">
-                <div className="m-4">
-                  <Image
-                    src={uy_about_us}
-                    width={180}
-                    alt="Picture of Lysander Uy"
-                  />
-                  <div className="flex flex-col align-items-center place-items-center justify-center">
-                    <p className="text-white font-bold text-lg py-3">
-                      Lysander Uy
-                    </p>
-                    <div className="flex gap-2">
-                      <a
-                        href="https://github.com/lysanderuy"
-                        target="_blank"
-                        title="GitHub"
-                      >
-                        <Github className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a href="mailto:lysander.uy@gmail.com" title="Gmail">
-                        <Mail className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                      <a
-                        href="https://www.linkedin.com/in/lysander-uy-805557223/"
-                        target="_blank"
-                        title="LinkedIn"
-                      >
-                        <Linkedin className="hover:animate-bounce hover:stroke-[#22C55E]" />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
-        <div className="py-3.5 place-items-center flex align-items-center justify-center">
-          <p className="text-xs text-gray-300 text-center pl-8 pr-8 py-4">
-            So sit back, relax, and let Klaro take the wheel. Your parcels are
-            in good hands...and maybe a few robot arms.
-          </p>
-        </div>
+
+        <Separator className="my-4" />
+
+        <p className="text-xs text-gray-300 text-center my-8">
+          So sit back, relax, and let Klaro take the wheel. Your parcels are in
+          good hands...and maybe a few robot arms.
+        </p>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/links.jsx
+++ src/app/links.jsx
@@ -20,3 +20,34 @@ export const NavItems = [
     for: "all",
   },
 ];
+
+export const TeamData = [
+  {
+    name: "Sean Aguilar",
+    image: "/aguilar_about_us.png",
+    github: "https://github.com/formesean",
+    mail: "mailto:seanaguilar698@gmail.com",
+    linkedin: "https://www.linkedin.com/in/seanaguilar04",
+  },
+  {
+    name: "Bea Canos",
+    image: "/canos_about_us.png",
+    github: "https://github.com/vaebelle",
+    mail: "mailto:beabelle.canos@gmail.com",
+    linkedin: "https://www.linkedin.com/in/beabellecanos",
+  },
+  {
+    name: "May Ochia",
+    image: "/ochia_about_us.png",
+    github: "https://github.com/Mayochia",
+    mail: "mailto:mayochia5@gmail.com",
+    linkedin: "https://www.linkedin.com/in/may-ochia-086267270/",
+  },
+  {
+    name: "Lysander Uy",
+    image: "/uy_about_us.png",
+    github: "https://github.com/lysanderuy",
+    mail: "mailto:lysander.uy@gmail.com",
+    linkedin: "https://www.linkedin.com/in/lysander-uy-805557223/",
+  },
+];
